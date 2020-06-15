45bd93a805d2c042aa1fbca76ef2bd5873d9c444
nurul-hidayati <66908420+nurul-hidayati@users.noreply.github.com>
2020-06-14 20:45:20 -0700

    Create if_else.java

---
+++ PERCABANGAN/if_else.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Takehome;
+
+/**
+ *
+ * @author HP
+ */
+public class if_else {
+    public static void main(String[] args) {
+        int x=5, y=10;
+        if(x<y){
+            System.out.println("x lebih kecil dari y");
+        }
+    }
+}
