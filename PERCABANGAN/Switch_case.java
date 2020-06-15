30159abb842094ff8f0e2a05c70e6bd9fe35ee3e
nurul-hidayati <66908420+nurul-hidayati@users.noreply.github.com>
2020-06-14 20:47:37 -0700

    Create Switch_case.java

---
+++ PERCABANGAN/Switch_case.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Takehome;
+
+/**
+ *
+ * @author HP
+ */
+public class Switch_case {
+    public static void main(String[] args) {
+        int bulan = 3;
+        switch(bulan){
+            case 1:
+                System.out.println("Januari");
+                break;
+            case 2:
+                System.out.println("Februari");
+                break;
+            case 3:
+                System.out.println("Maret");
+                break;
+            case 4:
+                System.out.println("April");
+                break;    
+            case 5:
+                System.out.println("Mei");
+            case 6:
+                System.out.println("Juni");
+            case 7:
+                System.out.println("Juli");
+            case 8:
+                System.out.println("Agustus");
+            case 9:
+                System.out.println("September");
+            case 10:
+                System.out.println("Oktober");
+            case 11:
+                System.out.println("November");
+            case 12:
+                System.out.println("Desember");
+        }
+    }
+}
