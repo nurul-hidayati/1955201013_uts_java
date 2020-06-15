eb4d4b19b3f0f56237858c49e2760fea73769cd2
nurul-hidayati <66908420+nurul-hidayati@users.noreply.github.com>
2020-06-14 20:58:45 -0700

    Create For.java

---
+++ PERULANGAN/For.java
@@ -0,0 +1,18 @@
+*/
+package Counted_Loop;
+
+/**
+ *
+ * @author HP
+ */
+public class For {
+    public static void main(String[] args) {
+        //int i;
+        for (int  i = 0; i < 10 ; i++){
+            System.out.print(i+" ");
+        }
+    }   
+}
+
+        //int variabel
+        //for (nilai awal; kondisi 2; kondisi 3)
