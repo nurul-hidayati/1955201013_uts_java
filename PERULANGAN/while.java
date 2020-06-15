a0f68b639650b8a3129d7276c1f01269e6c6691b
nurul-hidayati <66908420+nurul-hidayati@users.noreply.github.com>
2020-06-14 20:59:24 -0700

    Create while.java

---
+++ PERULANGAN/while.java
@@ -0,0 +1,21 @@
+ */
+package Counted_Loop;
+
+/**
+ *
+ * @author HP
+ */
+public class While {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+/*
+misal :
+        while (kondisi) {
+            kode yg akan dieksekusi
+*/
